d8174a24db92c460f3ef324fb874c08fe7d094e3
Khureltsooj <kj@shiftdsm.com>
2019-01-22 11:23:07 -0600

    initialize

---
+++ Design Pattern/VIPER Files.xctemplate/___FILEBASENAME___Presenter.swift
@@ -0,0 +1,23 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  ___COPYRIGHT___
+//
+
+import UIKit
+class ___VARIABLE_ModuleName___Presenter: ___VARIABLE_ModuleName___PresenterDelegate {
+
+    weak var view: ___VARIABLE_ModuleName___Delegate?
+    var interactor: ___VARIABLE_ModuleName___InteractorInputDelegate?
+    var router: ___VARIABLE_ModuleName___RouterDelegate?
+
+    func viewDidLoad() {
+
+    }
+}
+
+extension ___VARIABLE_ModuleName___Presenter: ___VARIABLE_ModuleName___InteractorOutputDelegate {
+
+}
